9ba3b79521cf5601d51cce12c011c3532449e72b
Francisco <javiermartincuenca@gmail.com>
2020-10-10 09:06:58 +0200

    Setup Header

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,77 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-white text-grey-10" bordered>
+      <q-toolbar class="constrain">
+        <q-btn
+          to="/camera"
+          icon="eva-camera-outline"
+          class="large-screen-only q-mr-sm"
+          size="18px"
+          flat
+          round
+          dense
+        />
+        <q-separator vertical spaced class="large-screen-only" />
+        <q-toolbar-title class="text-grand-hotel text-bold">
+          Quasargram
+        </q-toolbar-title>
+        <q-btn
+          to="/"
+          icon="eva-home-outline"
+          class="large-screen-only"
+          size="18px"
+          flat
+          round
+          dense
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-footer class="bg-white small-screen-only" bordered>
+      <q-tabs
+        class="text-grey-10"
+        active-color="primary"
+        indicator-color="transparent"
+      >
+        <q-route-tab icon="eva-home-outline" to="/" exact />
+        <q-route-tab icon="eva-camera-outline" to="/camera" />
+      </q-tabs>
+    </q-footer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'MainLayout',
+  data() {
+    return {};
+  }
+});
+</script>
+
+<style lang="scss">
+.q-footer {
+  .q-tab__icon {
+    font-size: 30px;
+  }
+}
+
+.q-toolbar__title {
+  @media (max-width: $breakpoint-xs-max) {
+    text-align: center;
+  }
+  font-size: 30px;
+}
+
+.q-toolbar {
+  @media (min-width: $breakpoint-sm-min) {
+    height: 77px;
+  }
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <h1>Home page</h1>
+  </q-page>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "HomePage"
+});
+</script>
