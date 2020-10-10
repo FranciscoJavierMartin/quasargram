7441fa7dd7ff07b8ca039553f03185ac8fba1c88
Francisco <javiermartincuenca@gmail.com>
2020-10-10 10:05:30 +0200

    Design home page

---
+++ src/interfaces/models.ts
@@ -0,0 +1,7 @@
+export interface IPost {
+  id: number;
+  caption: string;
+  date: number;
+  location: string;
+  imageUrl: string;
+}
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,113 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Home page</h1>
+  <q-page class="constrain q-pa-md">
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card
+          v-for="post in this.posts"
+          :key="post.id"
+          class="card-post q-mb-md"
+          flat
+          bordered
+        >
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label class="text-bold">Title</q-item-label>
+              <q-item-label caption>
+                {{ post.location }}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator />
+
+          <q-img :src="post.imageUrl" />
+
+          <q-card-section>
+            <div>{{ post.caption }}</div>
+            <div class="text-caption text-grey">
+              {{ post.date | niceDate }}
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-4 large-screen-only">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">nickname</q-item-label>
+            <q-item-label caption>
+              User name
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue from 'vue';
+import { date } from 'quasar';
 
 export default Vue.extend({
-  name: "HomePage"
+  name: 'HomePage',
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: 'Golden Gate Bridge',
+          date: 1602315139518,
+          location: 'San Francisco, United States',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg',
+        },
+        {
+          id: 2,
+          caption: 'Golden Gate Bridge',
+          date: 1602315139518,
+          location: 'San Francisco, United States',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg',
+        },
+        {
+          id: 3,
+          caption: 'Golden Gate Bridge',
+          date: 1602315139518,
+          location: 'San Francisco, United States',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg',
+        },
+        {
+          id: 4,
+          caption: 'Golden Gate Bridge',
+          date: 1602315139518,
+          location: 'San Francisco, United States',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg',
+        },
+      ],
+    };
+  },
+  filters: {
+    niceDate(value: string) {
+      return date.formatDate(value, 'D MMM YYYY');
+    },
+  },
 });
 </script>
+
+<style lang="scss">
+.card-post {
+  .q-img {
+    min-height: 200px;
+  }
+}
+</style>
